84e417b4789b410fb57ec098bf9fdd0a32e9c783
MarigonaN <marigonanuhija@gmail.com>
2020-06-15 16:47:24 +0200

    Update Recommend.jsx

--- linkedln-clone/src/Component/Content/Profile/Recommend.jsx
+++ linkedln-clone/src/Component/Content/Profile/Recommend.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Jumbotron, Container} from 'react-bootstrap';
+import {Jumbotron, Container, Row, Col} from 'react-bootstrap';
 
 const Recommend=(props) =>{
     
