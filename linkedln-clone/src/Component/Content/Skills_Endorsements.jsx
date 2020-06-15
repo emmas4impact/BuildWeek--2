1f6a8444144c8ab3f5667216d89395b4f098057e
MarigonaN <marigonanuhija@gmail.com>
2020-06-15 15:07:21 +0200

    Update Skills_Endorsements.jsx

--- linkedln-clone/src/Component/Content/Skills_Endorsements.jsx
+++ linkedln-clone/src/Component/Content/Skills_Endorsements.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { FaPlusCircle, FaChevronDown } from 'react-icons/fa';
 import {Container} from 'react-bootstrap'
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, Dropdown} from 'react-bootstrap'
 
-
+import './Skills_Endorsements.css'
 class Skills_Endorsements extends Component {
     render() {
         return (<>
@@ -29,11 +29,24 @@ class Skills_Endorsements extends Component {
                     </div>
                     </div>
                     <div class="card-header" id="headingOne">
+                   
                     <h5 class="mb-0" style={{textAlign: "center"}}>
-                        <button   class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                       Show More <FaChevronDown />
-                        </button>
+                    <div class="dropdown">
+                    <button type="button" >Show More <FaChevronDown /></button>
+                        <div class="dropdown-content">
+                        
+                        
+                        <h6 className="mt-4"><FaPlusCircle /> Full-Stack Development</h6>
+                       <hr></hr>
+                       <h6><FaPlusCircle /> Back-End Development</h6>
+                       <hr></hr>
+                       <h6><FaPlusCircle /> Front-End Development</h6>
+                       <hr></hr>
+                        </div>
+                        </div>
+                 
                     </h5>
+                
                     </div>
                     </div>
                     </div>
