19dd44a9c0296e049437274e7951c2f1e38b2ed9
Oksana <myzove@mail.ru>
2020-06-15 16:38:42 +0300

    adding font awesome

--- linkedln-clone/src/App.js
+++ linkedln-clone/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Component/NavBar/NavBar';
-// import FontAwesome from 'react-fontawesome'
-// import faStyles from 'font-awesome/css/font-awesome.css'
+import {  faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
     <>
     <NavBar />
+  
     </>
   );
 }
