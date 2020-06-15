8420d8d5718907105b99194646efbad88f4f28b4
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-15 15:06:35 +0300

    jumbotron added

--- linkedln-clone/src/Component/Content/Content.jsx
+++ linkedln-clone/src/Component/Content/Content.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import Profile from './Profiles'
+import ProfileForm from './Profile/Form'
 
 class Content extends Component{
     
     render(){
         return(
+            <>
             <Profile />
+            <ProfileForm />
+            </>
+            
         )
     }
 }
--- linkedln-clone/src/Component/Content/Profiles.jsx
+++ linkedln-clone/src/Component/Content/Profiles.jsx
@@ -1,12 +1,129 @@
 import React, {Component} from 'react';
-import styles from './Profile/profile.module.css';
+import styles from './Profile/Profile.module.css';
+import {BsPersonLinesFill } from "react-icons/bs";
+import {Form,InputGroup, List, Image, Col, Row, Button,Jumbotron, Container} from 'react-bootstrap';
 
+const url ="https://striveschool.herokuapp.com/api/profile/"
+
+const username ='user9'
+const password = 'sP4YMKhBpqQHAPJN'
+const headers =new Headers({
+    "Content-Type" : "application/json",
+    'Authorization': 'Basic ' + btoa(username + ":" + password),
+});
 
 
 class Profile extends Component{
+  
+        
+    
+    
+    componentDidMount(){
+        let response = fetch(url, {
+            method :'GET',
+            
+            headers: headers,
+        });
+        console.log(response);
+        return response;
+        
+        
+    }
+   
     render(){
+        
+        
         return(
-            <h1 className={styles.Profile}>Edit public profile & URL</h1>
+            <>
+                <div>
+                   
+                    <Row>
+                        <Col md={8} style={{backgroundColor: "white"}}>
+                             <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>Strengthen your profile</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                            
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>About</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>Activity</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>Your DashBoard</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                           
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <Button className="bg-primary">Take Skill quiz</Button>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>Recommendation</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>Accomplishment</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                           
+                            <Jumbotron fluid style={{backgroundColor: "white"}} className="shadow">
+                                <Container>
+                                    <h4>Interest</h4>
+                                    <p>
+                                    This is a modified jumbotron that occupies the entire horizontal space of
+                                    its parent.
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                        </Col>
+                        <Col md={4}>
+                                <p className="d-flex flex-column">Edit public profile & URL</p>
+                            <hr></hr>
+                            <a href="#"><p style={{color: "gray"}}>Add profile in another language</p></a>
+                        </Col>
+                    </Row>
+                   
+                </div>
+            </>
         )
     }
     
