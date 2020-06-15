6295ee0c78ad29b7ba3ee25d06cee20c04f5ab9a
MarigonaN <marigonanuhija@gmail.com>
2020-06-15 12:16:51 +0200

    Skills&Endorsements component

---
+++ linkedln-clone/src/Component/Content/Skills_Endorsements.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Skills_Endorsements extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Skills_Endorsements;
