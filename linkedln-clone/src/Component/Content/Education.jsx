1f94965f83c7fd52082ac35a9a6f6339c2f1900d
MarigonaN <marigonanuhija@gmail.com>
2020-06-16 12:15:44 +0200

    Update Education.jsx

--- linkedln-clone/src/Component/Content/Education.jsx
+++ linkedln-clone/src/Component/Content/Education.jsx
@@ -65,13 +65,78 @@ class Education extends Component {
                         </Row>
                         
                     </Row>
+
+
+
+                    <Row>
+                        <Col md={10}>
+                     <h4 >Licenses&Certifications</h4>
+                     </Col>
+                     <Col md={2}>
+                     <FaPlus />
+                     </Col>
+                     </Row>
+                    <Row >
+                        <Row>
+                            <Col md={4}>
+                            <img src="https://cdn3.iconfinder.com/data/icons/inficons/512/linkedin.png" style={{width: '50px'}}/>
+                            </Col>
+                        
+                        </Row>
+                        <Col md={8}>
+                            <Col>
+                            <h6>CSS: Selectors</h6>
+                            <p>LinkedIn</p>
+                            <p>Issued May 2020 No expiration Date</p>
+                            </Col>
+                        </Col>
+                        <Row>
+                       
+                        </Row>
+                        
+                    </Row>
+                    <Row>
+                    <Col md={10} >
+                        
+                       
+                       
+                        </Col>
+                    <Col md={2} >
+                        
+                        <FaPen  />
+                       
+                        </Col>
+                        </Row>
+                    <hr></hr>
+                    <Row >
+                        <Row>
+                            <Col md={4}>
+                            <img src="https://cdn3.iconfinder.com/data/icons/inficons/512/linkedin.png" style={{width: '50px'}}/>
+                            </Col>
+                        
+                        </Row>
+                        <Col md={8}>
+                            <Col>
+                            <h6>Computer Science Principles: Digital Information</h6>
+                            <p>LinkedIn</p>
+                            <p>Issued May 2020 No expiration Date</p>
+                            </Col>
+                        </Col>
+                        <Row>
+                       
+                        </Row>
+                        
+                    </Row>
                     
                 </Jumbotron>
+                
+                
             </Col>
             <Col md={4}>
                 
         
             </Col>
+            
         </Row>
         );
     }
