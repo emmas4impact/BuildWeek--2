30b1b076ebc37a0f3cc7dc517d0dcda1a9720470
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-17 17:47:42 +0300

    update

--- linkedln-clone/src/Component/Content/Jumbotron.jsx
+++ linkedln-clone/src/Component/Content/Jumbotron.jsx
@@ -29,15 +29,15 @@ this.setState({person:data, loading:false})
 }
 render(){
  return(
-    <Jumbotron fluid>
-    <Container>
-     
-      <Card>
+    <Jumbotron fluid className='box'>
+    <Container style={{padding: '0'}} className='ommit '>
+    
+      <Card style={{position: 'relative'}} style={{border: 'none'}}>
         <Card.Img variant="top" src="./jumboCover.jpeg" />
         <Card.Body>
-          <Card.Text>
-          {this.state.loading || !this.state.person ? (<div>loading...</div>):(<div> <div><Image src={this.state.person.image} roundedCircle style={{width: "60px"}}/></div><div> {this.state.person.id}</div>
-          <div>{this.state.person.bio}</div><div>{this.state.person.name}</div><div>{this.state.person.email}</div></div>)}
+        <Card.Text>
+          {this.state.loading || !this.state.person ? (<div>loading...</div>):(<div> <div><Image className='img-jumbo' src={this.state.person.image}/></div><div> {this.state.person.id}</div>
+          <div className='name pt-4'>{this.state.person.name}</div><div className='bio'>{this.state.person.bio}</div><div className='email'>{this.state.person.email}</div></div>)}
           </Card.Text>
         </Card.Body>
       </Card>
