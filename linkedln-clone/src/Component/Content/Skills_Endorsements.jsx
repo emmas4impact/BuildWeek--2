35a81166cb8c96726b32476c40b12ec63ca49be5
MarigonaN <marigonanuhija@gmail.com>
2020-06-17 10:46:49 +0200

    Another Commit

--- linkedln-clone/src/Component/Content/Skills_Endorsements.jsx
+++ linkedln-clone/src/Component/Content/Skills_Endorsements.jsx
@@ -16,54 +16,86 @@ class Skills_Endorsements extends Component {
                     <div class="card-body">
                        <h4>Skills&Endorsements</h4>
                        <Button id="quiz">Take Skill quiz</Button>   
-                       <h6 className="mt-4"><FaPlusCircle /> Full-Stack Development</h6>
+                       <h6 className="mt-4"><FaPlusCircle size="20px" /> Full-Stack Development</h6>
                        <hr></hr>
-                       <h6><FaPlusCircle /> Back-End Development</h6>
+                       <h6><FaPlusCircle size="20px" /> Back-End Development</h6>
                        <hr></hr>
-                       <h6><FaPlusCircle /> Front-End Development</h6>
+                       <h6><FaPlusCircle size="20px" /> Front-End Development</h6>
                        <hr></hr>
                        <p></p>
                        <p></p>
-                       <Accordion defaultActiveKey="0">
+                      
+                       
+                    
+                    </div>
+                    
+                    <Accordion defaultActiveKey="0" >
                         <Card>
-                            <Accordion.Toggle as={Card.Header} eventKey="0">
-                            See more...
+                            <Accordion.Toggle as={Card.Header} eventKey="0" className="text-center">
+                            See more <FaChevronDown />
                             </Accordion.Toggle>
                             <Accordion.Collapse eventKey="0">
                             <Card.Body>
-                                <ul> 
-                                    <li> Web Service developer</li>
-                                    <li>Web Services API</li>
-                                    <li>Algorithms and Data Structure</li>
-                                    <li>Software Developer</li>
-                                    <li>Automation Tester</li>
-                                    <li> backend developer.</li>
+                            <h4>Tools & Technologies</h4>
+                                        <hr></hr>
+                                <Row className="text-left">
                                 
+                                    <Col md={6}>
+                                        
+                                <ul > 
+                                    <li><FaPlusCircle size="20px" /> Java Script</li>
+                                    <li><FaPlusCircle size="20px" /> React JS</li>
+                                    <li><FaPlusCircle size="20px" /> Node JS</li>
+                                    <li><FaPlusCircle size="20px" /> HTML</li>
+                                    <li><FaPlusCircle size="20px" /> CSS</li>
+                                    
                                 </ul>
+                                </Col>
+                                <Col md={6} >
+                                <ul > 
+                                    <li><FaPlusCircle size="20px" /> Java Script</li>
+                                    <li><FaPlusCircle size="20px" /> React JS</li>
+                                    <li><FaPlusCircle size="20px" /> Node JS</li>
+                                    <li><FaPlusCircle size="20px" /> HTML</li>
+                                    <li><FaPlusCircle size="20px" /> CSS</li>
+                                    
+                                </ul>
+                                </Col>
+                                </Row>
+                                <h4>Languages</h4>
+                                        <hr></hr>
+                                <Row>
+                                     <Col md={12}>
+                                        <ul > 
+                                          <li><FaPlusCircle size="20px" /> English</li>
+                                           
+                                        </ul>
+                                      </Col>
+                                 </Row>
+                                 <h4>Other Skills</h4>
+                                        <hr></hr>
+                                <Row>
+                                <Col md={6}>
+                                        <ul > 
+                                            <li><FaPlusCircle size="20px" /> Gulp</li>
+                                            <li><FaPlusCircle size="20px" /> Animation.css</li>
+                                           
+                                        </ul>
+                                        </Col>
+                                        <Col md={6} >
+                                        <ul > 
+                                            <li><FaPlusCircle size="20px" /> Modernizer.js</li>
+                                            
+                                        </ul>
+                                        </Col>
+                                 </Row>
+                                
                              </Card.Body>
                             </Accordion.Collapse>
                         </Card>
                         
                     </Accordion>
-                    </div>
-                    
-                  
-                    <h5 class="mb-1" style={{textAlign: "center"}}>
-                    <div class="dropdown">
-                    <button type="button" >Show More <FaChevronDown /></button>
-                        <div class="dropdown-content">
-                        
-                        
-                        <h6 className="mt-3"><FaPlusCircle /> Java Script</h6>
-                       <hr></hr>
-                       <h6><FaPlusCircle /> React JS</h6>
-                       <hr></hr>
-                       <h6><FaPlusCircle /> Node JS</h6>
-                       <hr></hr>
-                        </div>
-                        </div>
-                 
-                    </h5>
+                   
                 
                     </div>
                     </div>
