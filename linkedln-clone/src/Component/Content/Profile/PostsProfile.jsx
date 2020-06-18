35314f746a9cd45dda172f7a039c30d71a96d6da
Oksana <myzove@mail.ru>
2020-06-18 16:02:18 +0300

    adding style

--- linkedln-clone/src/Component/Content/Profile/PostsProfile.jsx
+++ linkedln-clone/src/Component/Content/Profile/PostsProfile.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Router, withRouter} from 'react-router-dom';
 import  { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-import { FaPersonBooth, FaCamera, FaPhotoVideo, FaPaperPlane, FaShare, FaComment, FaPenFancy, FaInfo, FaDotCircle } from 'react-icons/fa';
+
+import { FaHashtag, FaCamera, FaPhotoVideo, FaPaperPlane, FaShare, FaComment, FaPenFancy, FaInfo, FaDotCircle, FaSquare, FaBookmark, FaNetworkWired } from 'react-icons/fa';
 class PostsProfile extends Component {
     render() {
         return (
@@ -13,58 +14,55 @@ class PostsProfile extends Component {
                         <div className='row'>
                             <div className='col-12'>
                                 <Card>
-                                    <FaPersonBooth />
-                                    <Card.Body>
-                                        
-                                        <Card.Text>
-                                        <Card.Title>Welcome, NAME!</Card.Title>
-                                        <smal>Update your profile</smal>
+                                   
+                                    <Card.Body className='head-prof'>
+                                  
+                                           <img className='mb-2' style={{width: '50px', borderRadius: '50%' }} src='https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png' />
+                                        <Card.Text style={{lineHeight: '0.5'}}>
+                                        <h6 style={{fontWeight: '700'}}>Welcome, NAME!</h6>
+                                        <a style={{ color: '#0073B1', fontSize: '12px'}}>Update your profile</a>
                                         </Card.Text>
                                     </Card.Body>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem>
-                                            <p>Connections
-                                            <small>90</small></p>
-                                            <p>Grow your network</p>
-                                            <p>Who viewed your profile
-                                            <small>47</small></p>
+                                    <ListGroup className="list-group-flush" style={{fontSize: '12px', fontWeight: '700'}}>
+                                        <ListGroupItem style={{lineHeight: '0.5'}}>
+                                            <p className='pt-2' style={{lineHeight: '0.1', color: '#666666', display: 'flex', justifyContent: 'space-between'}}>Connections
+                                            <small style={{fontWeight: '700', fontSize: '13px', color: '#0073B1'}}>90</small></p>
+                                            <p className='pb-2'>Grow your network</p>
+                                            <p style={{color: '#666666', display: 'flex', justifyContent: 'space-between'}}>Who viewed your profile
+                                            <small style={{fontWeight: '700', fontSize: '13px', color: '#0073B1' }}>47</small></p>
                                         </ListGroupItem>
                                         <ListGroupItem>See all Premium features</ListGroupItem>
-                                        <ListGroupItem>Saved items</ListGroupItem>
+                                        <ListGroupItem><FaBookmark className='mr-2'/>Saved items</ListGroupItem>
                                  
                                     </ListGroup>
                                    
                                 </Card>
                             </div>
                             <div className='col-12 mt-2'>
-                               <Card>
+                               <Card style={{fontSize: '12px'}}>
                                    <Card.Body>
-                                       <ul>Recent
-                                           <li>Premium</li>
-                                           <li>#programminglife</li>
-                                           <li>#codinglife</li>
-                                           <li>#learntocode</li>
-                                           <li>React learning Gro..</li>
+                        
+                                       <p style={{fontWeight: '500'}}>Recent</p>
+                                       <ul className='list-unstyled' style={{color: '#666666', fontWeight: '700'}}>
+                                           <li><FaNetworkWired /> Premium Career Group</li>
+                                           <li><FaHashtag />  programminglife</li>
+                                           <li><FaHashtag />  codinglife</li>
+                                           <li><FaHashtag />  learntocode</li>
+                                           <li><FaNetworkWired />  React learning Gro..</li>
                                        </ul>
-                                       <ul>Recent
-                                           <li>Premium</li>
-                                           <li>#programminglife</li>
-                                           <li>#codinglife</li>
-                                           <li>#learntocode</li>
-                                           <li>React learning Gro..</li>
-                                       </ul>
-                                       <p>Events <small>+</small></p>
-                                       <ul>Follwed Hashtags
-                                           <li>#programming life</li>
-                                           <li>#coding life</li>
-                                           <li>#codinglife</li>
-                                           <li>#learntocode</li>
+                                       <p style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontWeight: '700', color: '#0073B1' }}>Events <small style={{fontSize: '20px', fontWeight: '500'}}>+</small></p>
+                                       <p style={{fontWeight: '700', color: '#0073B1' }}>Followed Hashtags</p>
+                                       <ul className='list-unstyled' style={{color: '#666666', fontWeight: '700'}}>
+                                           <li><FaHashtag />  programming life</li>
+                                           <li><FaHashtag />  coding life</li>
+                                           <li><FaHashtag />  codinglife</li>
+                                           <li><FaHashtag />  learntocode</li>
                                            <li>See all</li>
                                        </ul>
 
                                    </Card.Body>
                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem>
+                                        <ListGroupItem style={{textAlign: 'center', fontWeight: '700'}}>
                                             Discover more
                                         </ListGroupItem>
                                    </ListGroup>
@@ -79,7 +77,7 @@ class PostsProfile extends Component {
                                 <Card>
                                     <Card.Body style={{display: 'flex', justifyContent: 'space-between'}}>
                                         <textarea style={{flex: '0.8'}} placeholder=" &#xf044; Start a post"></textarea>
-                                      
+                                      <faKey style={{color: '#000'}}/>
                                         <div>
                                             <button className='btn-upload'><FaCamera /></button>
                                             <button className='btn-upload ml-5 left-border'><FaPhotoVideo /></button>
