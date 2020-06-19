cfc90f24cb1f9c951e33c51dc4c1fbe8f0b07427
Oksana <myzove@mail.ru>
2020-06-19 12:24:38 +0300

    adding style to modal

--- linkedln-clone/src/Component/Content/Profile/PostsProfile.jsx
+++ linkedln-clone/src/Component/Content/Profile/PostsProfile.jsx
@@ -239,7 +239,7 @@ class PostsProfile extends Component {
                                 return(
                                     <Card body key={i} className='mt-2'> 
                                     <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                    <p><img src={user.user.image} style={{width: "40px", borderRadius: "50px", marginRight: "10px"}}/> {user.user.name}</p>
+                                    <p style={{fontWeight: '700', fontSize: '14px'}}><img src={user.user.image} style={{width: "40px", height: '40px', borderRadius: "50%", marginRight: "10px"}}/> {user.user.name}</p>
 
                                     
                                     <Dropdown>
