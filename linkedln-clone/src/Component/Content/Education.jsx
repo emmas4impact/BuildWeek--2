f005352f7eb29652ba3c2fe14aa2acd169efeb8a
Oksana <myzove@mail.ru>
2020-06-19 11:59:03 +0300

    adding form

--- linkedln-clone/src/Component/Content/Education.jsx
+++ linkedln-clone/src/Component/Content/Education.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Jumbotron, Container, Row, Col, Image} from 'react-bootstrap'
+import {Jumbotron, Container, Row, Col, Image, Modal, Button, } from 'react-bootstrap'
 import { FaPlus, FaPen, FaAlignJustify } from "react-icons/fa";
 
 
@@ -8,15 +8,27 @@ const password="sP4YMKhBpqQHAPJN";
 
 
 class Education extends Component {
+    
     state = {
         loading:true,
          profile:null,
          person:[],
+         
+         showModal: false
           
-     };
-
-
-
+     }
+     open =() => {
+        this.setState({ showModal: true });
+      }
+  
+     getInitialState =() =>{
+        return { showModal: false };
+      }
+    
+      close =() => {
+        this.setState({ showModal: false });
+      }
+    
 
    componentDidMount=async()=>{
         const url="https://striveschool.herokuapp.com/api/profile/"+this.props.match.params.username+"/experiences";
@@ -32,14 +44,72 @@ class Education extends Component {
         this.setState({person:data[0], loading:false})
 }
     render() {
+       
+
+
         console.log("EDUCATION", this.props.match.params.username)
         return (
         <Row>
             <Col md={8} >
                 <Jumbotron style={{backgroundColor: "White"}} className='box jumbo-prof-1'>
                 <Row>
-                        <Col md={2} >
+                    <Col md={12} >
+                     <div className='exper-btn'>
                      <h4>Experience</h4>
+                     <button onClick={this.open} style={{background: 'none', border: 'none', fontSize: '25px', fontWeight: '400', color: '#0073B1'}}>+</button>
+                     </div>
+                     <Modal show={this.state.showModal} onHide={this.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add education</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <form>
+             
+                        <div className="form-group">
+                        <label for="school">School *</label>
+                        <input type="text" className="form-control" id="school" placeholder="Ex: Boston Univerity" required/>
+                        </div>
+                        <div className="form-group">
+                        <label for="degree">Degree</label>
+                        <input type="text" className="form-control" id="degree" placeholder="Degree" />
+                        </div>
+              
+                    <div className="form-group">
+                        <label for="fieldOfStudy">Field of study</label>
+                        <input type="text" className="form-control" id="fieldOfStudy" placeholder="Ex: Business" />
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                        <label for="inputState">Start year</label>
+                        <select id="inputState" class="form-control">
+                        <option selected>Year</option>
+                        <option>...</option>
+                         </select>
+                        </div>
+
+                       
+                        <div className="form-group col-md-6">
+                        <label for="degree">End Year or expected</label>
+                        <select id="degree" class="form-control">
+                        <option selected>Year</option>
+                        <option>...</option>
+                         </select>
+                        </div>
+                   </div>
+                   <div className="form-group">
+                        <label for="grade">Field of study</label>
+                        <input type="text" className="form-control" id="grade"/>
+                    </div>
+                  
+                </form>
+                      
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.close}>Save</Button>
+                    </Modal.Footer>
+                    </Modal>
+      
                      {this.state.loading || !this.state.person ? (<div>loading...</div>):(<div> <div><Image src={this.state.person.image} rounded style={{width: "0px"}}/></div></div>)}
                      {this.state.loading || !this.state.person ? (<div>loading...</div>):(<div> <div><Image src={this.state.person.image} rounded style={{width: "60px", marginTop: "10px"}}/></div></div>)}
                      </Col>
