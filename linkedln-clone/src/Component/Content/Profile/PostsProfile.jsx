d2a9406d49b577794b4c23925194d250e2e9337a
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-24 13:05:08 +0300

    Update PostsProfile.jsx

--- linkedln-clone/src/Component/Content/Profile/PostsProfile.jsx
+++ linkedln-clone/src/Component/Content/Profile/PostsProfile.jsx
@@ -41,9 +41,8 @@ class PostsProfile extends Component {
     }
 
     open = async(postId) => {
-        const username="user29";
-        const password="w4X9FKLNUDSXwzYu";
-        const url="https://linkedln-backend.herokuapp.com/posts/" + postId 
+    
+        const url="https://linkedln-backend.herokuapp.com/api/posts/" + postId 
         const response= await fetch(url,{
           method:'GET'
           
@@ -128,7 +127,11 @@ class PostsProfile extends Component {
         const datas= await responses.json();
         console.log(datas);
         this.setState({person:datas, loading:false})
+        
+        //this.postStatus()
+        //this.deleteStatus()
         }
+   
         sendPost=(e)=>{
             const newPost = this.state.sendStatus
             newPost.text = e.currentTarget.value
@@ -191,10 +194,10 @@ class PostsProfile extends Component {
    
     }
     
-    editStatus = async ()=>{
+    editStatus = async (postId)=>{
         
        
-        const url="https://linkedln-backend.herokuapp.com/api/posts/"+this.state.postId
+        const url="https://linkedln-backend.herokuapp.com/api/posts/"+postId
         const response= await fetch(url,{
           method:'PUT',
           body: JSON.stringify(this.state.sendStatus),
